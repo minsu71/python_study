dc483813842421f3856db57dfcb52ebd1ceb3068
minsu71 <alstn8344@gmail.com>
2025-07-09 12:09:21 +0900

    example

--- python/ex17.py
+++ python/ex17.py
@@ -1,19 +1,37 @@
 import random
 
-num1 = random.randint(0, 10)
-num2 = random.randint(0, 10)
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
 x = random.randint(0,3)
 
 
 if x ==0:
-    sum = int(num1+num2)
+    sum = num1+num2
+    reply = int(input("{0}+{1}의 값은?" .format(num1, num2)))
+    if reply == sum:
+        print("정답입니다.")
+    else:
+        print("틀렸습니다.")
 elif x==1:
-    sum = int(num1-num2)
-elif x==2:
-    sum = int(num1*num2)
-else:
-    sum = int(num1/num2)
+    sum = num1-num2
+    reply = int(input("{0}-{1}의 값은?" .format(num1, num2)))
+    if reply == sum:
+        print("정답입니다.")
+    else:
+        print("틀렸습니다.")
 
-print(num1, num2)
-print(x)
-print(sum)
+elif x==2:
+    sum = num1*num2
+    reply = int(input("{0}*{1}의 값은?" .format(num1, num2)))
+    if reply == sum:
+        print("정답입니다.")
+    else:
+        print("틀렸습니다.")
+
+else:
+    sum = num1/num2
+    reply = int(input("{0}/{1}의 값은?" .format(num1, num2)))
+    if reply == sum:
+        print("정답입니다.")
+    else:
+        print("틀렸습니다.")
